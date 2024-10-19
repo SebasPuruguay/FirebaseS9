d98ffa21197c0609e2cbad77647893b0b78bde24
j <o>
2024-10-19 12:12:16 -0500

    autenticacion con firebase

---
+++ app/src/main/java/com/example/firebases9/LoginActivity.kt
@@ -0,0 +1,53 @@
+package com.example.firebases9
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val  auth = FirebaseAuth.getInstance()
+
+        btnLogin.setOnClickListener {
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this){
+                    task->
+                    if(task.isSuccessful){
+                        Snackbar.make(findViewById(R.id.main),
+                            "Login correcto",
+                            Snackbar.LENGTH_SHORT).show()
+                        startActivity(Intent(this, MainActivity::class.java))
+                    }else{
+                        Snackbar.make(findViewById(R.id.main),
+                            "Login incorrecto",
+                            Snackbar.LENGTH_SHORT).show()
+
+
+                    }
+                }
+        }
+    }
+}
